9d8542511577f59af68c42963b36dc044e38f595
Baarosh <norbert.lipinski96@gmail.com>
2020-04-05 00:48:13 +0200

    v1

---
+++ Multiplication Tables.js
@@ -0,0 +1,26 @@
+/* Create a function that accepts dimensions, of Rows x Columns, as parameters in order to create a multiplication table sized according to the given dimensions. **The return value of the function must be an array, and the numbers must be Fixnums, NOT strings.
+
+Example:
+
+multiplication_table(3,3)
+
+1 2 3
+2 4 6
+3 6 9
+
+-->[[1,2,3],[2,4,6],[3,6,9]]
+
+Each value on the table should be equal to the value of multiplying the number in its first row times the number in its first column. */
+function multiplicationTable(row, col) {
+  let arr = new Array(row).fill(0)
+  arr = arr.map((c) => new Array(col).fill(0))
+  arr.forEach((c, ind) => {
+    for (let i = 0; i < c.length; i++) {
+      c[i] = (ind + 1) * (i + 1)
+    }
+  })
+  return arr
+}
+
+console.log(multiplicationTable(3, 3))
+console.log(multiplicationTable(3, 4))
