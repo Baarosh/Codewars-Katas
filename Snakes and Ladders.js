31a5da33d0bfeece616a864cd38915b996527b27
Baarosh <norbert.lipinski96@gmail.com>
2020-04-04 20:10:42 +0200

    v1

--- Snakes and Ladders.js
+++ Snakes and Ladders.js
@@ -27,10 +27,12 @@ class SnakesLadders {
     if (pos === 49) return 11
     if (pos === 51) return 67
     if (pos === 62) return 19
+    if (pos === 64) return 60
     if (pos === 71) return 91
     if (pos === 74) return 53
     if (pos === 78) return 98
     if (pos === 87) return 94
+    if (pos === 89) return 68
     if (pos === 92) return 88
     if (pos === 95) return 75
     if (pos === 99) return 80
