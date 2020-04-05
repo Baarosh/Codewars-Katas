151a4fe70a3f5b2f3f3ca9d3c29e56054ddec3c8
Baarosh <norbert.lipinski96@gmail.com>
2020-04-05 20:52:57 +0200

    v1

---
+++ Sort the odd.js
@@ -0,0 +1,29 @@
+/* You have an array of numbers.
+Your task is to sort ascending odd numbers but even numbers must be on their places.
+
+Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+Example
+
+sortArray([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4] */
+
+function sortArray(array) {
+  if (array.length === 0) return []
+  const indexes = []
+  let nums = []
+  array.forEach((el, i) => {
+    if (el % 2 == 1) {
+      indexes.push(i)
+      nums.push(el)
+    }
+  })
+  nums = nums.sort((a, b) => a - b)
+
+  indexes.forEach((el, i) => {
+    array[el] = nums[i]
+  })
+
+  return array
+}
+
+console.log(sortArray([5, 3, 2, 8, 1, 4]))
