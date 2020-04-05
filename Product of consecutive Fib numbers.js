108d8df90569583e0c63ffd93de6b8b46e771ffa
Baarosh <norbert.lipinski96@gmail.com>
2020-04-05 15:22:58 +0200

    v1

---
+++ Product of consecutive Fib numbers.js
@@ -0,0 +1,52 @@
+/* The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(m) verifying F(m) * F(m+1) = prodyou will return
+
+[F(m), F(m+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(m) being the smallest one such as F(m) * F(m+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55 */
+
+function productFib(prod) {
+  const fib = (num) => {
+    if (num === 0) return 0
+    if (num === 1) return 1
+    return fib(num - 2) + fib(num - 1)
+  }
+  const tab = [0, 0]
+
+  for (let i = 2; tab[0] * tab[1] < prod; i++) {
+    tab.push(fib(i))
+    tab.shift()
+  }
+
+  if (tab[0] * tab[1] === prod) tab.push(true)
+  else tab.push(false)
+
+  return tab
+}
+
+console.log(productFib(714))
+console.log(productFib(800))
