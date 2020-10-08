ec3f46336865534be4a325be0e849bef16a06f4e
Baarosh <norbert.lipinski96@gmail.com>
2020-10-08 15:57:43 +0200

    v1

--- Count the Digit.js
+++ Count the Digit.js
@@ -23,6 +23,6 @@ function nbDig(n, d) {
 }
 
 console.log(nbDig(5750, 0), 4700)
-console.log(nbDig(11011, 2), 9481)
-console.log(nbDig(12224, 8), 7733)
-console.log(nbDig(11549, 1), 11905)
+// console.log(nbDig(11011, 2), 9481)
+// console.log(nbDig(12224, 8), 7733)
+// console.log(nbDig(11549, 1), 11905)
